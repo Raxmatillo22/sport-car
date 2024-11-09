0599f1aefe34fe80d1a1a73834ba6cd4c6f65c6b
Raxmatillo <rustamovraxmatillo07@gmail.com>
2024-11-09 15:43:37 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// toggle menu button
+function toggleMenu() {
+  const menu = document.querySelector(".menu");
+  const nav = document.querySelector(".nav");
+  menu.classList.toggle("active");
+  nav.classList.toggle("active");
+}
+
+//Change the background video when click on the gallery images
+function changeVideo(name) {
+  const bgVideoList = document.querySelectorAll(".bgVideo");
+}
